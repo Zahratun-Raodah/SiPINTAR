4f3242499adfb0074a3982fee4b4dae2e54c0731
Zahratun-Raodah <zahratunraodah34@gmail.com>
2025-08-04 00:08:54 +0700

    Update tambah.blade.php

---
+++ resources/views/jurnal_mengajar/tambah.blade.php
@@ -0,0 +1,62 @@
+@extends('index')
+
+@section('content')
+<div class="section-header mt-4">
+    <h1 class="mb-4">Halaman Tambah</h1>
+</div>
+<div class="section-body">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-6 col-lg">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Tambah Jurnal Mengajar</h4>
+                </div>
+                <form action="{{ route('jurnal.store') }}" method="POST">
+                    @csrf
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label>Jam Ke-</label>
+                            <input type="number" class="form-control" name="jam_ke" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Kelas</label>
+                            <select class="form-control" name="kelas" required>
+                                <option value="" hidden>--pilih kelas--</option>
+                                @foreach(['7A','7B','7C','7D','7E','7F','8A','8B','8C','8D','8E','8F','9A','9B','9C','9D','9E'] as $kelas)
+                                    <option value="{{ $kelas }}" {{ (isset($data) && $data->kelas == $kelas) ? 'selected' : '' }}>{{ $kelas }}</option>
+                                @endforeach
+                            </select>
+                        </div>                        
+                        <div class="form-group">
+                            <label>Kompetensi dasar</label>
+                            <input type="text" class="form-control" name="kompetensi_dasar" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Kegiatan Belajar</label>
+                            <input type="text" class="form-control" name="kegiatan_belajar" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Absensi Sakit</label>
+                            <input type="number" class="form-control" name="absen_sakit" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Absensi Izin</label>
+                            <input type="number" class="form-control" name="absen_izin" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Absensi Alpa</label>
+                            <input type="number" class="form-control" name="absen_alpha" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Keterangan</label>
+                            <input type="text" class="form-control" name="keterangan" required>
+                        </div>
+                        <div>
+                            <button type="submit" class="btn btn-primary">tambah</button>
+                        </div>
+                    </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+@endsection
